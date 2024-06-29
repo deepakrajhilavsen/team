a5240448951b170f95551e6061c4a4f45ec68c0e
lohitha1978 <lohitha.hilavsen@gmail.com>
2024-06-29 16:18:27 +0530

    Updated login,signin and profile

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
+import React from "react";
 import { MdAccountCircle } from "react-icons/md";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='navbar flex  justify-between items-center p-[3rem]'>
-      <div className="logoDiv"><h1 className="logo text-[25px] text-blueColor"><strong>Job</strong>Portal</h1></div>
+    <div className="navbar flex  justify-between items-center p-[3rem]">
+      <div className="logoDiv">
+        <h1 className="logo text-[25px] text-blueColor">
+          <strong>Job</strong>Portal
+        </h1>
+      </div>
       <div className="menu flex gap-8">
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor">
+          Companies
+        </li>
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">blog</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor">
+          Contact
+        </li>
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor">
+          {" "}
+          <Link
+            to="/signup"
+            className="menuList text-[#6f6f6f] hover:text-blueColor"
+          >
+            Signup
+          </Link>
+        </li>
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor">
+          <Link to="/login">Login</Link>
+        </li>
         <li className="menuList flex items-center text-[#6f6f6f] hover:text-blueColor">
-  <MdAccountCircle className="mr-2" />
-  <Link to="/profile">Profile</Link>
-</li>
-
-        
-       
-       
+          <MdAccountCircle className="mr-2" />
+          <Link to="/profile">Profile</Link>
+        </li>
       </div>
-     
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
