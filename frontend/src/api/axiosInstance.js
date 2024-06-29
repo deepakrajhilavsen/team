e38b46a6147509ac6a79f815f430c0c3d35ab769
lohitha1978 <lohitha.hilavsen@gmail.com>
2024-06-29 15:52:22 +0530

    login,signin and profilepage  update

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { axiosErrorHandler } from '../utils/axiosErrorHandler';
 
 const instance = axios.create({
-  baseURL: "https://e87d-2401-4900-7b72-1de0-40a1-c09c-d999-9a9c.ngrok-free.app",
+  baseURL: "process.env.REACT_APP_AXIOS_BASE_URL",
   timeout: 10000,
 });
 
